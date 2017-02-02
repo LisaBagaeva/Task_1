693bfe566a70785763dde45e0feb129972c03ac1
Lizzi <Lizzi>
2017-02-02 04:05:02 +0300

    Update method printNonUniqueSymbols

--- src/Task_1.java
+++ src/Task_1.java
@@ -1,30 +1,29 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Task_1 {
 	public static void printNonUniqueSymbols(String expression) {
-		char[] chars = expression.toCharArray();
-		StringBuffer out = new StringBuffer();
-		char currentChar = 0;
-		int index = 0;
-		int lb = 1;
-		while (index != chars.length - 1) {
-			for (int i = lb; i < chars.length; i++) {
-				if (chars[index] == ' ')
-					break;
-				if (chars[index] == chars[i]) {
-					if (chars[index] != currentChar) {
-						out = out.append(chars[index]);
-						currentChar = chars[index];
-					}
-					chars[i] = ' ';
-				}
-			}
-			index++;
-			lb++;
+		if (!expression.isEmpty()) {
+			char[] chars = expression.toCharArray();
+			Map<Character, Boolean> elements = new HashMap<>();
+			for (Character ch : chars)
+				if (ch != ' ')
+					if (elements.containsKey(ch))
+						elements.replace(ch, true);
+					else
+						elements.put(ch, false);
+
+			Set<Character> keys = elements.keySet();
+			for (Character ch : keys)
+				if (elements.get(ch))
+					System.out.print(ch + " ");
 		}
-		System.out.println(out);
 	}
 
 	public static void main(String[] argv) {
-		String exp = "       1      1     242    3     1111145  6    455255    7    54444 2";
+		String exp = "1444      443124555        65";
 		Task_1.printNonUniqueSymbols(exp);
 	}
 }
